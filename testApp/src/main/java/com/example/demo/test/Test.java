85dd362f764215a96d244817611c754357ff5c4a
Dejan Gajic <gaja@antecna.com>
2021-03-06 12:01:04 +0100

    first commit from development

--- testApp/src/main/java/com/example/demo/test/Test.java
+++ testApp/src/main/java/com/example/demo/test/Test.java
@@ -1,4 +1,20 @@
 package com.example.demo.test;
 
 public class Test {
+    private Integer id;
+
+    public Test() {
+    }
+
+    public Test(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
